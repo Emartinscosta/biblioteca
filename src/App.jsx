7b897efc6a031a3412a4bf3ea9f8ed839b0c212a
Emartinscosta <emartins.costa@hotmail.com>
2024-12-10 14:45:09 -0300

    Adicionar livro

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function App() {
   const [livros, setLivros] = useState([]);
+  const { handleSubmit, register, reset} = useForm();
+
 
   async function loadata() {
     const resposta = await fetch("http://localhost:3000/livros");
@@ -27,6 +30,17 @@ function App() {
   loadata();
 
   }
+
+  async function salvarLivro(dados) {
+    await fetch("http://localhost:3000/livros", {
+      method: "Post",
+      headers: {"content-type": "application/json"},
+      body:JSON.stringify({dados})
+    });
+    loadata();
+    reset();
+    
+  }
   
 
   useEffect(() => {
@@ -35,7 +49,44 @@ function App() {
 
   return (
     <div>
-      <h1>Livros</h1>
+      <h1><strong> Livros </strong></h1>
+
+      <form onSubmit={handleSubmit(salvarLivro)}>
+        <div>
+          <label htmlFor="titulo">Titulo</label> <br />
+          <input type="text" id="titulo" {...register("titulo")} />
+        </div>
+        <div>
+          <label htmlFor="autor">Autor</label> <br />
+          <input type="text" id="autor" {...register("autor")} />
+        </div>
+        <div>
+          <label htmlFor="paginas">Paginas</label> <br />
+          <input type="text" id="paginas" {...register("paginas")} />
+        </div>
+        <div>
+          <label htmlFor="categoria">Categoria</label> <br />
+          <select id="categoria" {...register("categoria")}>
+            <option value="Ação">Ação</option>
+            <option value="Terror">Terror</option>
+            <option value="Tecnologia">Tecnologia</option>
+            <option value="Romance">Romance</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="dataPublicacao">Data de Publicacao</label> <br />
+          <input type="text" id="dataPublicacao" {...register("dataPublicacao")} />
+        </div>
+        <div>
+          <label htmlFor="isbn">ISBN</label> <br />
+          <input type="text" id="isbn" {...register("isbn")} />
+        </div>
+        <button>
+          Adicionar
+        </button>
+      </form> <br />
+
+      
 
       <table>
         <tbody>
